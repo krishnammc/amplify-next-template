1dce2f014d0534eb58ba8e54e85bdc39f967fcf2
krishnammc <krishnamural048@gmail.com>
2024-09-24 20:11:22 +0530

    updated alignment

--- app/page.tsx
+++ app/page.tsx
@@ -389,26 +389,26 @@ export default function App() {
 
 
   return (
-        <Flex bg={"#fff"} flexDir={"row"} w={"100%"} maxW={"100vw"}>
+        <Flex bg={"#fff"} flexDir={"row"} w={"100%"} maxW={"100vw"} justifyContent={"center"} alignItems={"center"}>
      
     <Authenticator   socialProviders={["google", "facebook"]}  components={components}>
       {({ signOut, user }) => (
         <main>
           <h1>My todos</h1>
-          <h1>{user?.signInDetails?.loginId}'s todos</h1>
-          <button onClick={createTodo}>+ new</button>
+          <button onClick={createTodo} style={{color:"black"}}>+ new</button>
           <ul>
             {todos.map((todo) => (
               <li key={todo.id} onClick={() => deleteTodo(todo.id)} style={{cursor:"pointer"}} >{todo.content}</li>
             ))}
           </ul>
-          <div> 
+          <div>
             🥳 App successfully hosted. Try creating a new todo.
             <br />
             <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
               Review next steps of this tutorial.
             </a>
           </div>
+          <Button > edit </Button>
           <button style={{color:'black'}} onClick={signOut}>Sign out</button>
       
         </main>

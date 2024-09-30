b22018de70b439987b29d6c136aea8bde17aee64
krishnammc <krishnamural048@gmail.com>
2024-09-30 20:20:34 +0530

    updated signup 5

--- lib/pre_login/pages/signup.tsx
+++ lib/pre_login/pages/signup.tsx
@@ -33,17 +33,17 @@ interface SignUpForm extends HTMLFormElement {
 }
 
 
-export const awsConfig = {
-  // ... other configurations ...
-  Auth: {
-    mandatorySignIn: true,
-    // ... other auth configurations ...
-  },
-  attributeMapping: {
-    'custom:company_name': 'company_name',
-    'custom:role': 'role',
-  },
-};
+// export const awsConfig = {
+//   // ... other configurations ...
+//   Auth: {
+//     mandatorySignIn: true,
+//     // ... other auth configurations ...
+//   },
+//   attributeMapping: {
+//     'custom:company_name': 'company_name',
+//     'custom:role': 'role',
+//   },
+// };
 
 
 const SignUpPage = () => {
